7c909fa1405ccb1fd5e0da725170d2b9314b50f9
Mark Utting <utting@usc.edu.au>
2019-11-14 18:27:54 +1000

    Tell ReadTheDocs about the starting point.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ author = 'Mark Utting'
 
 # The full version, including alpha/beta/rc tags
 release = '0.3.2'
-
+master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 
