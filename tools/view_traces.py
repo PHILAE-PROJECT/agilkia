80dd1df47409f7cacc92a728250c2c1da8c3c37e
Mark Utting <m.utting@uq.edu.au>
2022-05-12 16:39:14 +1000

    Add -v verbose flag to print event-to-char map.  Add --ok NUM flag so can specify that 200 is ok for web requests.

--- tools/view_traces.py
+++ tools/view_traces.py
@@ -20,11 +20,11 @@ import agilkia
 INDENT = "   "  # prefix for each trace (plus one extra space)
 
 
-def make_action_status_table(df: pd.DataFrame) -> pd.DataFrame:
+def make_action_status_table(df: pd.DataFrame, ok=0) -> pd.DataFrame:
     """From TraceSet DataFrame, creates a table of Actions showing how many got Ok vs Error."""
-    ok = df[df.Status == 0].groupby("Action").size()
-    err = df[df.Status != 0].groupby("Action").size()
-    data = pd.DataFrame({"Ok": ok, "Err": err})
+    good = df[df.Status == ok].groupby("Action").size()
+    err = df[df.Status != ok].groupby("Action").size()
+    data = pd.DataFrame({"Ok": good, "Err": err})
     data.fillna(0, inplace=True, downcast="infer")
     data["Total"] = data.Ok + data.Err
     totals = pd.DataFrame([data.sum().rename("Total")])
@@ -36,12 +36,16 @@ def main():
     """A command line program that prints a set of traces, plus some summary statistics."""
     parser = argparse.ArgumentParser(description=__doc__,
         formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument("-v", "--verbose", help="show more information, such as the event-to-char map.",
+        action="store_true")
     parser.add_argument("-e", "--eventchars", help="a csv file containing an event-to-char map.")
     parser.add_argument("-n", "--noclusters", help="view traces in-order, without clusters.",
         action="store_true")
     parser.add_argument("-r", "--repeats", help="compress repeated events with action=REPEATS")
     parser.add_argument("-s", "--status", help="color events with non-zero status red, to highlight errors",
         action="store_true")
+    parser.add_argument("--ok", help="specify the status value that represents success (default 0).", 
+        default="0", metavar="NUM", type=int)
     parser.add_argument("traceset", help="an Agilkia traceset file (*.json)")
     args = parser.parse_args()
     # print(f"Args are:", args)
@@ -65,18 +69,23 @@ def main():
     else:
         for tr in traceset.traces:
             print(INDENT, tr.to_string(compress=repeats, color_status=args.status))
+    if args.verbose:
+        print("==== event chars ====")
+        ev_chars = traceset.get_event_chars()
+        for action,ch in ev_chars.items():
+            print(f"    {action:20s}    {ch}")
     print("==== statistics ====")
     df = traceset.to_pandas()
     statuses = df.Status.value_counts()
-    percent_ok = 100.0 * statuses.get(0, 0) / df.shape[0]
+    percent_ok = 100.0 * statuses.get(args.ok, 0) / df.shape[0]
     # print(df.head())
     print(f"Number of traces     : {len(traceset.traces)}")
     print(f"Average trace length : {df.groupby('Trace').count().Action.mean():.2f}")
     print(f"Number of clusters   : {traceset.get_num_clusters()}")
     print(f"Number of events     : {df.shape[0]}")
     print(f"Number of event kinds: {len(actions)}")
-    print(textwrap.indent(str(make_action_status_table(df)), "    "))
-    print(f"Percent of status=0  : {percent_ok:.2f}%")
+    print(textwrap.indent(str(make_action_status_table(df, ok=args.ok)), "    "))
+    print(f"Percent of status=ok : {percent_ok:.2f}%")
     error_counts = df.groupby("Error").Action.count()
     if len(error_counts) > 1:
         print(f"Categories of errors : ({100.0 - percent_ok:.2f}% total)")
